7e2b1ec4f3c89dd3d1d3d1d03bc60f2f7532fcce
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2021-11-16 21:28:37 +0100

    chore: add global utils

---
+++ src/ft_global_utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_global_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asabani <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 18:42:35 by asabani           #+#    #+#             */
+/*   Updated: 2021/11/16 01:07:21 by asabani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = -1;
+	while (*(s + ++i))
+		if (*(s + i) == (char)c)
+			return ((char *)s + i);
+	if (c == '\0')
+		return ((char *)s + i);
+	return ((char *)0);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	length;
+
+	length = 0;
+	while (str[length])
+		length++;
+	return (length);
+}
+
+size_t	ft_count_digits(unsigned long num, int base)
+{
+	size_t	digits;
+
+	digits = 0;
+	while (num != 0)
+	{
+		digits++;
+		num /= base;
+	}
+	return (digits);
+}
+
+void	ft_putchar(const char c)
+{
+	write(1, &c, 1);
+}
