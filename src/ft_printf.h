9ab90cdd6858996dd7bd3e1a4e8e12beedf6e91a
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2021-11-19 01:29:06 +0100

    chore: add ft_printf header file

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: asabani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 10:56:20 by asabani           #+#    #+#             */
-/*   Updated: 2021/11/16 21:51:00 by asabani          ###   ########.fr       */
+/*   Updated: 2021/11/19 00:22:12 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,28 @@ typedef enum e_spec_type
 	UNKNWON_SPEC='\0'
 }	t_spec_type;
 
+typedef struct s_flags
+{
+	int	left_justify;
+	int	zero_padded;
+	int	sign;
+	int	blank;
+	int	hash;
+}	t_flags;
+
 typedef struct s_spec
 {
+	t_flags		flags;
+	size_t		width;
+	int			prec;
+	size_t		length;
 	t_spec_type	spec_type;
 }	t_spec;
 
 void	reset_spec(t_spec *spec);
+int		check_flags(t_spec *spec, const char *fmt);
+int		check_minimum_width(t_spec *spec, const char *fmt);
+int		check_maximum_width(t_spec *spec, const char *fmt);
 int		check_spec_type(t_spec *spec, const char *fmt);
 
 // global utils prototype
@@ -58,11 +74,14 @@ typedef struct s_arg
 	char	*data;
 	size_t	length;
 }	t_arg;
-t_arg	ft_get_char(const char c);
-t_arg	ft_get_string(const char *str);
-t_arg	ft_get_address(const void *ptr);
-t_arg	ft_get_number(int number);
-t_arg	ft_get_number_base(unsigned int number, int radix, char *base);
-t_arg	ft_get_percent(void);
+t_arg	ft_get_char(const unsigned char c, t_spec *spec);
+t_arg	ft_get_string(const char *str, t_spec *spec);
+t_arg	ft_get_address(const void *ptr, t_spec *spec);
+t_arg	ft_get_number(int number, t_spec *spec);
+t_arg	ft_get_number_base(unsigned long int number, int radix, t_spec *spec);
+t_arg	ft_get_percent(t_spec *spec);
 t_arg	ft_get_unknown(char c);
+char	*string_justify(const char *str, size_t length, int amount, \
+		int left_justify);
+size_t	get_minimum(size_t a, size_t b);
 #endif
