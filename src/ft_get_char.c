b44631633841004fdd48d41bc1eee058ab295d62
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2021-11-19 01:26:20 +0100

    chore: add ft_get_char

--- src/ft_get_char.c
+++ src/ft_get_char.c
@@ -6,21 +6,29 @@
 /*   By: asabani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 12:05:39 by asabani           #+#    #+#             */
-/*   Updated: 2021/11/16 01:48:04 by asabani          ###   ########.fr       */
+/*   Updated: 2021/11/18 12:12:30 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-t_arg	ft_get_char(const char c)
+t_arg	ft_get_char(const unsigned char c, t_spec *spec)
 {
 	t_arg	arg;
+	int		amount;
+	char	str[2];
 
-	arg.data = (char *)malloc(sizeof(char) * 2);
-	if (!arg.data)
-		return (arg);
+	str[0] = c;
+	str[1] = '\0';
 	arg.length = 1;
-	arg.data[0] = c;
-	arg.data[1] = '\0';
+	amount = spec->width - 1;
+	if (amount <= 0)
+		arg.data = string_justify(str, 1, 0, spec->flags.left_justify);
+	else
+	{
+		arg.data = string_justify(str, 1, amount, spec->flags.left_justify);
+		arg.length += amount;
+	}
 	return (arg);
 }
