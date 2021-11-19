9271847071c3d581bff33b33bbb635a4a531eca5
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2021-11-19 01:37:21 +0100

    chore: add ft_get_number \(uncompleted\)

--- src/ft_get_number.c
+++ src/ft_get_number.c
@@ -6,61 +6,152 @@
 /*   By: asabani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 12:21:11 by asabani           #+#    #+#             */
-/*   Updated: 2021/11/16 15:25:17 by asabani          ###   ########.fr       */
+/*   Updated: 2021/11/19 01:33:12 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
-#include <limits.h>
+#include <string.h>
 
-t_arg	ft_get_number(int number)
+int	get_maximum(int a, int b)
 {
-	t_arg		arg;
-	long int	nb;
-	size_t		length;
+	if (a > b)
+		return (a);
+	return (b);
+}
 
-	nb = number;
-	length = 0;
-	if (nb <= 0)
-	{
-		nb *= -1;
-		length++;
-	}
-	length += ft_count_digits(nb, 10);
-	arg.data = (char *)malloc(sizeof(char) * (length + 1));
-	if (!arg.data)
-		return (arg);
-	arg.length = length;
-	arg.data[length] = '\0';
+void	numcpy(char *str, char *base, \
+		unsigned long int number, size_t length)
+{
+	int	radix;
+
+	radix = ft_strlen(base);
 	while (length--)
-	{	
-		arg.data[length] = DECIMAL_BASE[nb % 10];
-		nb /= 10;
+	{
+		str[length] = base[number % radix];
+		number /= radix;
 	}
-	if (number < 0)
-		arg.data[0] = '-';
+}
+
+t_arg	number_justify(unsigned long int number, \
+		size_t	length, int amount, t_spec *spec)
+{
+	t_arg	arg;
+	char	*base;
+
+	base = LC_HEX_BASE;
+	if (spec->spec_type == UNSIGNED_SPEC || \
+			spec->spec_type == DECIMAL_SPEC || spec->spec_type == INTEGER_SPEC)
+		base = DECIMAL_BASE;
+	else if (spec->spec_type == UC_HEX_SPEC)
+		base = UC_HEX_BASE;
+	arg.length = length;
+	arg.data = (char *)malloc(sizeof(char) * (length + 1));
+	if (spec->flags.left_justify)
+	{
+		numcpy(arg.data, base, number, length - amount);
+		memset(arg.data + (length - amount), ' ', amount);
+	}
+	else
+	{
+		if (spec->flags.zero_padded)
+			memset(arg.data, '0', amount);
+		else
+			memset(arg.data, ' ', amount);
+		numcpy(arg.data + amount, base, number, length - amount);
+	}
 	return (arg);
 }
 
-t_arg	ft_get_number_base(unsigned int number, int radix, char *base)
+void	add_sign(t_arg *arg, t_spec *spec, int amount, int isnegative)
+{
+	char	sign;
+
+	sign = '\0';
+	if (isnegative)
+		sign = '-';
+	else if (spec->flags.blank)
+		sign = ' ';
+	else if (spec->flags.sign)
+		sign = '+';
+	if (arg->data && sign)
+	{
+		if (spec->flags.left_justify || spec->flags.zero_padded)
+			arg->data[0] = sign;
+		else
+			arg->data[amount] = sign;
+	}
+}
+
+t_arg	ft_get_number(int number, t_spec *spec)
+{
+	t_arg		arg;
+	size_t		length;
+	int			amount;
+	long int	nb;
+
+	length = 0;
+	nb = number;
+	if (number <= 0)
+		nb *= -1;
+	if (spec->flags.sign || spec->flags.blank)
+		length++;
+	if (spec->spec_type == POINTER_SPEC || (spec->flags.hash && number))
+		length += 2;
+	length += ft_count_digits(nb, 10);
+	if (spec->prec)
+		length = get_maximum(spec->length, length);
+	if (number <= 0)
+		length++;
+	amount = spec->width - length;
+	if (amount < 0)
+		amount = 0;
+	length += amount;
+	arg = number_justify(nb, length, amount, spec);
+	// handle sign 
+	//add_sign(&arg, spec, amount, number < 0 ? 1: 0);
+	arg.data[length] = '\0';
+	return (arg);
+}
+
+void	add_prefix(t_arg *arg, t_spec *spec, int amount)
+{
+	char	prefix;
+
+	prefix = 'x';
+	if (spec->spec_type == UC_HEX_SPEC)
+		prefix = 'X';
+	if (arg->data)
+	{
+		if (spec->flags.left_justify || spec->flags.zero_padded)
+			arg->data[1] = prefix;
+		else
+			arg->data[amount +1] = prefix;
+	}
+}
+
+t_arg	ft_get_number_base(unsigned long int number, int radix, t_spec *spec)
 {
 	t_arg	arg;
 	size_t	length;
+	int		amount;
 
 	length = 0;
 	if (number == 0)
 		length++;
+	if (spec->spec_type == POINTER_SPEC || (spec->flags.hash && number))
+		length += 2;
 	length += ft_count_digits(number, radix);
-	arg.data = (char *)malloc(sizeof(char) * (length + 1));
-	arg.length = length;
-	if (!arg.data)
-		return (arg);
+	if (spec->prec)
+		length = get_maximum(spec->length, length);
+	amount = spec->width - length;
+	if (amount < 0)
+		amount = 0;
+	length += amount;
+	arg = number_justify(number, length, amount, spec);
+	if (spec->spec_type == POINTER_SPEC || (spec->flags.hash && number))
+		add_prefix(&arg, spec, amount);
 	arg.data[length] = '\0';
-	while (length--)
-	{
-		arg.data[length] = base[number % radix];
-		number /= radix;
-	}
 	return (arg);
 }
