183d678aead9ffdbea37b7240fc540df92390fbe
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2021-11-19 01:26:51 +0100

    chore: add ft_get_address

--- src/ft_get_address.c
+++ src/ft_get_address.c
@@ -6,35 +6,20 @@
 /*   By: asabani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 01:48:54 by asabani           #+#    #+#             */
-/*   Updated: 2021/11/16 15:20:17 by asabani          ###   ########.fr       */
+/*   Updated: 2021/11/18 20:24:45 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-t_arg	ft_get_address(const void *ptr)
+t_arg	ft_get_address(const void *ptr, t_spec *spec)
 {
 	t_arg			arg;
-	size_t			length;
 	unsigned long	addr;
 
-	addr = (unsigned long)ptr;
-	length = ft_count_digits(addr, 16) + 2;
-	if (!ptr)
-		length++;
-	arg.data = (char *)malloc(sizeof(char) * (length + 1));
-	if (!arg.data)
-		return (arg);
-	arg.length = length;
-	arg.data[length--] = '\0';
-	while (addr != 0)
-	{
-		arg.data[length--] = LC_HEX_BASE[addr % 16];
-		addr /= 16;
-	}
-	arg.data[0] = '0';
-	arg.data[1] = 'x';
-	if (!ptr)
-		arg.data[2] = '0';
+	addr = 0;
+	if (ptr)
+		addr = (unsigned long)ptr;
+	arg = ft_get_number_base(addr, 16, spec);
 	return (arg);
 }
