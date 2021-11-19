2aa00c96d408767ec97df0abf61656854aff3ee9
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2021-11-19 01:28:20 +0100

    chore: add ft_parser_utils

--- src/ft_get_string.c
+++ src/ft_get_string.c
@@ -6,38 +6,66 @@
 /*   By: asabani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 01:49:16 by asabani           #+#    #+#             */
-/*   Updated: 2021/11/16 17:22:45 by asabani          ###   ########.fr       */
+/*   Updated: 2021/11/19 01:18:49 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <string.h>
+#include <stdio.h>
 
-t_arg	ft_get_string(const char *str)
+size_t	get_minimum(size_t a, size_t b)
 {
-	t_arg	arg;
-	size_t	i;
-	size_t	length;
+	if (a < b)
+		return (a);
+	return (b);
+}
 
-	if (!str)
+void	space_filler(char *str, int amount)
+{
+	int	i;
+
+	i = -1;
+	while (++i < amount)
+		*(str + i) = ' ';
+}
+
+char	*string_justify(const char *str, \
+		size_t length, int amount, int left_justify)
+{
+	char	*new_str;
+
+	new_str = (char *)malloc(sizeof(char) * (length + amount + 1));
+	if (left_justify)
 	{
-		arg.data = strdup("(null)");
-		arg.length = 6;
+		memcpy(new_str, str, length);
+		memset(new_str + length, ' ', amount);
 	}
 	else
 	{
-		length = ft_strlen(str);
-		arg.data = (char *)malloc(sizeof(char) * (length + 1));
-		if (!arg.data)
-			return (arg);
-		arg.length = length;
-		i = 0;
-		while (i < length)
-		{
-			arg.data[i] = str[i];
-			i++;
-		}
-		arg.data[i] = '\0';
+		memset(new_str, ' ', amount);
+		memcpy(new_str + amount, str, length);
 	}
+	new_str[length + amount] = '\0';
+	return (new_str);
+}
+
+t_arg	ft_get_string(const char *str, t_spec *spec)
+{
+	t_arg	arg;
+	size_t	length;
+	int		amount;
+
+	if (!str)
+		str = "(null)";
+	length = ft_strlen(str);
+	if (spec->prec)
+		length = get_minimum(spec->length, length);
+	arg.length = length;
+	amount = spec->width - length;
+	if (amount <= 0)
+		amount = 0;
+	arg.length += amount;
+	arg.data = string_justify(str, length, amount, spec->flags.left_justify);
 	return (arg);
 }
